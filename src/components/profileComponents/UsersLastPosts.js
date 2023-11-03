dc26cea236d4d00a97b4d9e5301a50a11f992ba5
Tornike Despotashvili <despo@Tornikes-MacBook-Air.local>
2023-11-03 14:31:00 +0400

    added lazy load to users posts

--- src/components/profileComponents/UsersLastPosts.js
+++ src/components/profileComponents/UsersLastPosts.js
@@ -1,35 +1,66 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 import axios from 'axios'
 import MapSingleUserPost from './mapSingleUserPost'
 
 export default function UsersLastPosts() {
 
     const [data, setData] = useState([])
+    const [visibleElement, setvisibleElement] = useState(10)
+        const [loading, setLoading] = useState(false)
+        const pageSize = 10
+        const postsContentRef = useRef(null);
 
-    useEffect(()=>{
-        const fetchUserPosts = async () => {
-            try {
-                const response = await axios.get('http://localhost:3300/posts/:id', ({withCredentials: true}))
-                setData(response.data)
-            } catch (error) {
-                console.log(error);
+        const handleScroll = () => {
+            const div = postsContentRef.current;
+            const scrollPosition = div.scrollTop;
+            const scrollHeight = div.scrollHeight;
+            const clientHeight = div.clientHeight;
+        
+            if (scrollPosition + clientHeight >= scrollHeight - 100 && !loading) {
+            
+                setLoading(true);
+        
+                setTimeout(() => {
+                setvisibleElement((prevvisibleElement) => prevvisibleElement + pageSize);
+                setLoading(false);
+            }, 0); 
             }
-        }
+        };
 
-        fetchUserPosts()
-    },[])
+        useEffect(() => {
+            const div = postsContentRef.current;
+            div.addEventListener("scroll", handleScroll);
+            return () => {
+            div.removeEventListener("scroll", handleScroll);
+            };
+            // eslint-disable-next-line
+        }, []);
+
+        useEffect(()=>{
+            const fetchUserPosts = async () => {
+                try {
+                    const response = await axios.get('http://localhost:3300/posts/:id', ({withCredentials: true}))
+                    setData(response.data)
+                } catch (error) {
+                    console.log(error);
+                }
+            }
+
+            fetchUserPosts()
+        },[])
 
   return (
     <div className='user_posts'>
         <p className='user_posts_header'>User's last posts</p>
 
-        <div className='last_posts_content'>
+        <div className='last_posts_content' ref={postsContentRef}>
 
-            { data ? 
-                <MapSingleUserPost data={data} />
-                    : 
-                'Loading......'
-            }
+        {
+        data ?
+            <MapSingleUserPost data={data.slice(0, visibleElement)} /> 
+            :
+            'Loading......'
+        }
             
         </div>
     </div>
